e3ff2e28983c075928936b0ef72dac266d37cbd3
lydiob7 <lydiob7@tutanota.com>
2024-07-16 08:39:54 -0300

    Initial Commit

---
+++ Sudoku/SudokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SudokuApp.swift
+//  Sudoku
+//
+//  Created by Tomi Scattini on 16/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct SudokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
