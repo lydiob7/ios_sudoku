5e9f436a2b8047cb8877de4d561cc78318f54e4f
lydiob7 <lydiob7@tutanota.com>
2024-08-07 14:30:43 -0300

    Feature: add readme file

---
+++ README.md
@@ -0,0 +1,5 @@
+#  iOS Sudoku
+## Crated by **Lydiob7 (Tomi Scattini)**
+
+This is just a simple Sudoku game I'm using to learn Swift and SwiftUI, feel free to use it, fork it, suggest changes or critizise my way of thinking.
+
--- Sudoku/models/Sudoku.swift
+++ Sudoku/models/Sudoku.swift
@@ -7,49 +7,6 @@
 
 import Foundation
 
-final class SavedSudoku: Codable {
-    var availableHints: Int
-    var currentState: [[Int]]
-    var difficulty: Difficulty?
-    var errorsCount: Int
-    var history: [[[Int]]]
-    var initialState: [[Int]]
-    var isPaused: Bool
-    var maxOfHints: Int
-    var maxOfMistakes: Int
-    var score: Int
-    var solution: [[Int]]
-    var time: TimeInterval
-    
-    init(
-        availableHints: Int,
-        currentState: [[Int]],
-        difficulty: Difficulty,
-        errorsCount: Int,
-        history: [[[Int]]],
-        initialState: [[Int]],
-        isPaused: Bool,
-        maxOfMistakes: Int,
-        maxOfHints: Int,
-        score: Int,
-        solution: [[Int]],
-        time: TimeInterval
-    ) {
-        self.availableHints = availableHints
-        self.currentState = currentState
-        self.difficulty = difficulty
-        self.errorsCount = errorsCount
-        self.history = history
-        self.initialState = initialState
-        self.isPaused = isPaused
-        self.maxOfHints = maxOfHints
-        self.maxOfMistakes = maxOfMistakes
-        self.score = score
-        self.solution = solution
-        self.time = time
-    }
-}
-
 struct Sudoku {
     let solution: [[Int]]
     let initialState: [[Int]]
